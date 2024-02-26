fbaa9bdf057f8e2fbae72bed9f4e3cbab98edfd5
Shivam Choudhary <shivm.c97@gmail.com>
2024-02-26 20:31:03 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treats all JS code as new version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3);  // code readability should be high
+console.log("Shivam");
+
+let name = "Shivam"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);  // type => undefined
+console.log(typeof null);   // type => object
